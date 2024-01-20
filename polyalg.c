264d651daba077e4258c689d1f4274d31b68f001
msharipov <msharipovr@gmail.com>
2024-01-20 11:53:55 -0500

    fixed GF28_mult

--- polyalg.c
+++ polyalg.c
@@ -5,15 +5,19 @@
 
 uint8_t GF28_mult(uint8_t f, uint8_t g) {
 
-    uint8_t result = 0;
+    uint8_t result = 0,
+            fxn = f; // fxn = f * x^n
 
     for (uint8_t n = 0; n < 8; n++) {
-        if (g >> n & 1U){
-            // If nth bit of g is set
 
-
-            result ^= f << n;
+        // If nth bit of g is set add nth power of f
+        if (g >> n & 1U) {
+            result ^= fxn;
         }
+
+        // Multiply fxn by x
+        fxn = fxn << 1 ^ (fxn & 0x80 ? 0x1B : 0);
+
     }
 
     return result;
