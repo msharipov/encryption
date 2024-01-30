3883194d2158d4b7c2b7845dcd3075e08329aed3
msharipov <msharipovr@gmail.com>
2024-01-30 10:34:43 -0500

    rearranged order of tests to match source code

--- tests/polyalg_tests.c
+++ tests/polyalg_tests.c
@@ -442,6 +442,39 @@ _Bool test_poly_add_mult(void) {
     return fail;
 }
 
+_Bool test_poly_add_ord(void) {
+    _Bool fail = 0;
+
+    int64_t a0[7] = {2, 4, -2, 3, 0, 0, 0},
+            exp0[7] = {0, 0, 2, 4, -2, 3, 0};
+
+    poly_add_ord(a0, 2, 6);
+    if (!poly_same(a0, exp0, 6)) {
+        fail = 1;
+        printf("Failed poly_add_ord test 0.\n");
+    }
+
+    int64_t a1[7] = {1, 2, 3, 4, 5, 6, 7},
+            exp1[7] = {1, 2, 3, 4, 5, 6, 7};
+
+    poly_add_ord(a1, 0, 6);
+    if (!poly_same(a1, exp1, 6)) {
+        fail = 1;
+        printf("Failed poly_add_ord test 1.\n");
+    }
+
+    int64_t a2[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9},
+            exp2[9] = {0, 0, 1, 2, 3, 4, 5, 8, 9};
+
+    poly_add_ord(a2, 2, 6);
+    if (!poly_same(a2, exp2, 8)) {
+        fail = 1;
+        printf("Failed poly_add_ord test 2.\n");
+    }
+
+    return fail;
+}
+
 _Bool test_poly_mult(void) {
     _Bool fail = 0;
 
@@ -495,39 +528,6 @@ _Bool test_poly_mult(void) {
         printf("Failed poly_mult test 3.\n");
         printf("out: "), print_array(c3, 20);
         printf("exp: "), print_array(exp3, 20);
-    }
-
-    return fail;
-}
-
-_Bool test_poly_add_ord(void) {
-    _Bool fail = 0;
-
-    int64_t a0[7] = {2, 4, -2, 3, 0, 0, 0},
-            exp0[7] = {0, 0, 2, 4, -2, 3, 0};
-
-    poly_add_ord(a0, 2, 6);
-    if (!poly_same(a0, exp0, 6)) {
-        fail = 1;
-        printf("Failed poly_add_ord test 0.\n");
-    }
-
-    int64_t a1[7] = {1, 2, 3, 4, 5, 6, 7},
-            exp1[7] = {1, 2, 3, 4, 5, 6, 7};
-
-    poly_add_ord(a1, 0, 6);
-    if (!poly_same(a1, exp1, 6)) {
-        fail = 1;
-        printf("Failed poly_add_ord test 1.\n");
-    }
-
-    int64_t a2[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9},
-            exp2[9] = {0, 0, 1, 2, 3, 4, 5, 8, 9};
-
-    poly_add_ord(a2, 2, 6);
-    if (!poly_same(a2, exp2, 8)) {
-        fail = 1;
-        printf("Failed poly_add_ord test 2.\n");
     }
 
     return fail;
